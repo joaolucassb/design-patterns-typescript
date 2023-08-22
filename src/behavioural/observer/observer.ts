b13b66a15908b7717b98640075f76275edad9531
Joao Lucas <joaolucasbezerra02@gmail.com>
2023-08-21 22:56:12 -0300

    Observer

---
+++ src/behavioural/observer/observer.ts
@@ -0,0 +1,90 @@
+interface Observable {
+  subscribe(...observers: Observer[]): void;
+  unsubscribe(obeserver: Observer): void;
+  notify(): void;
+}
+
+interface Observer {
+  update(...args: unknown[]): void;
+}
+
+// Concrete Observable
+class InputObservable implements Observable {
+  private observers: Observer[] = [];
+
+  constructor(public element: HTMLInputElement) {}
+
+  subscribe(...observers: Observer[]): void {
+    observers.forEach((observer) => {
+      if (!this.observers.includes(observer)) this.observers.push(observer);
+    });
+  }
+
+  unsubscribe(obeserver: Observer): void {
+    const obeserverIndex = this.observers.indexOf(obeserver);
+
+    if (obeserverIndex !== -1) {
+      this.observers.splice(obeserverIndex, 1);
+    }
+  }
+
+  notify(): void {
+    this.observers.forEach((observer) => observer.update(this));
+  }
+}
+
+// Concrete Observer
+class ParagraphObserver implements Observer {
+  constructor(public element: HTMLParagraphElement) {}
+
+  update(observable: Observable): void {
+    if (observable instanceof InputObservable) {
+      this.element.innerText = observable.element.value;
+    }
+  }
+}
+
+// Concrete Observer
+class DivObserver implements Observer {
+  constructor(public element: HTMLParagraphElement) {}
+
+  update(observable: Observable): void {
+    if (observable instanceof InputObservable) {
+      this.element.innerText = observable.element.value;
+    }
+  }
+}
+
+// Client Code
+function makeInput(): HTMLInputElement {
+  const input = document.createElement('input');
+  document.body.appendChild(input);
+  return input;
+}
+
+function makeParagraph(): HTMLParagraphElement {
+  const p = document.createElement('p');
+  document.body.appendChild(p);
+  p.innerText = 'Texto inicial (HardCoded)';
+  return p;
+}
+
+function makeDiv(): HTMLParagraphElement {
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+  div.innerText = 'Texto da Div';
+  return div;
+}
+
+const input = new InputObservable(makeInput());
+const p1 = new ParagraphObserver(makeParagraph());
+const p2 = new ParagraphObserver(makeParagraph());
+const div1 = new DivObserver(makeDiv());
+
+input.subscribe(p1, p2, div1);
+
+input.element.addEventListener('keyup', function () {
+  input.notify();
+});
+
+input.unsubscribe(p2);
